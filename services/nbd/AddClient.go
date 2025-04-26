7b3a55fe30bd26a635d52e70e1cb46e43a7ae900
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2025-04-27 01:43:11 +0700

    fix add client

--- services/nbd/AddClient.go
+++ services/nbd/AddClient.go
@@ -1,27 +1,29 @@
 package nbd
 
 import (
-	"os/exec"
 	"github.com/gin-gonic/gin"
+	"github.com/m4xkub/capstonev2_worker/services/utils"
 )
 
 type AddClientRequest struct {
 	IP string `json:"ip"`
 }
 
-func AddClient(c *gin.Context)  {
+func AddClient(c *gin.Context) {
 	allowListPath := "/etc/nbd-server/allowlist"
 	var req AddClientRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
-	
-	cmd := exec.Command("echo", req.IP, ">>", allowListPath)
-	output, err := cmd.Output()
+
+	err := utils.RunCommand("sudo", "echo", req.IP, ">>", allowListPath)
+
+	//cmd := exec.Command("echo", req.IP, ">>", allowListPath)
+	//output, err := cmd.Output()
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(200, gin.H{"message": string(output)})
+	c.JSON(200, gin.H{"message": "Client added successfully!"})
 }
